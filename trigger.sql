08a2f0aed29e0885e267c5adc21d0a9b065f460b
kricka91 <kristoffer.hallqvist@gmail.com>
2014-03-21 18:26:16 +0100

    Delete trigger.sql

--- trigger.sql
+++
@@ -1,9 +0,0 @@
-DELIMITER |
-
-CREATE TRIGGER increase_numThreads AFTER INSERT ON threads
-	FOR EACH ROW BEGIN
-		UPDATE categories SET numThreads = numThreads+1 WHERE id = NEW.category;
-	END
-|
-
-DELIMITER ;
