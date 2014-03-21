2557fde5e6518dc14e0fc693fabba233a9e6801a
kricka91 <kristoffer.hallqvist@gmail.com>
2014-03-21 18:24:20 +0100

    Delete banner.php

--- banner.php
+++
@@ -1,8 +0,0 @@
-<div class="top">
-	<a href="./frontpage.php"><img class="banner" src="./img/banner.png" /></a>
-	<div style="position:relative; width:250px; height:90px; margin-left:auto; margin-right:20px; top: 15px; background-color:white;">
-		<?php
-			include 'ucp.php';
-		?>
-	</div>
-</div>
